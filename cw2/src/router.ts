1516fc9d74ebfc16eec36ccf3329afdb6a317ee2
herbert3456 <hubert.bochenekty@interia.pl>
2026-03-14 12:01:18 +0100

    kkk

--- cw2/src/router.ts
+++ cw2/src/router.ts
@@ -5,11 +5,19 @@ export const handleRoute = (req:IncomingMessage, res:ServerResponse) => {
         res.writeHead(200, { "Content-Type": "text/plain" });
         res.end("Hello World!");
     }
-
     else if(req.url === "/info" && req.method === "GET") {
         res.writeHead(200, { "Content-Type": "text/plain" });
         res.end("This is the info page.");
     }
-
+    else if (req.url === "/json" && req.method === "GET") {
+        res.writeHead(200, { "Content-Type": "application/json" });
+        res.end(JSON.stringify({ 
+            message: "This is a JSON response.",
+            timestamp: new Date().toISOString()}));
+}
+    else {
+        res.writeHead(404, { "Content-Type": "text/plain" });
+        res.end("This page does not exist.");
+    }
 
 }
