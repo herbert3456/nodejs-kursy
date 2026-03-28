a74baa828ff1239817456571578de483cbff52c5
herbert3456 <hubert.bochenekty@interia.pl>
2026-03-28 09:29:16 +0100

    kk

--- cw3/src/FileRepo.ts
+++ cw3/src/FileRepo.ts
@@ -1,6 +1,7 @@
 import fs from 'fs/promises';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import type { Student } from './student.js';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const pathToPublic = path.join(__dirname,'..', 'public');
 //klasa do obsługi plików JSON
@@ -12,9 +13,25 @@ export class FileRepo {
 getfile(){
     return this.pathToFile;
 } 
-getAllStudents = async () => {
+getAllStudents = async (): Promise<Student[]> => {
     const data = await fs.readFile(this.pathToFile, "utf-8");
     return JSON.parse(data);
+}
+getStudentById = async (id: number): Promise<Student | null> => {
+    //pobranie wszystkich studentów z pliku
+    const students = await this.getAllStudents();
+    //znalezienie studenta o podanym id lun null jeśli nie istnieje
+    return students.find(student => student.id === id) || null;
+   
+}
+saveStudents = async (students: Student[]): Promise<void> => {
+    const data = JSON.stringify(students, null, 2);
+    await fs.writeFile(this.pathToFile, data, "utf-8");
 
-
+}
+addStudent = async (student: Student): Promise<void> => {
+    const students = await this.getAllStudents();
+    students.push(student);
+    await this.saveStudents(students);
+}
 }
--- cw3/src/router.ts
+++ cw3/src/router.ts
@@ -3,6 +3,8 @@ import fs from 'fs/promises';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { FileRepo } from "./FileRepo.js";
+import type { Student } from "./student.js";
+
 const router = Router();
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const pathToPublic = path.join(__dirname,'..', 'public');
@@ -27,4 +29,32 @@ router.get('/api/students', async (req: Request, res:Response) => {
     console.log(students);
     res.status(200).json(students);
 });
+router.get('/api/students/:id', async (req: Request, res:Response) => {
+    const id = parseInt(req.params.id as string);
+    if (isNaN(id)) {
+        res.status(400).json({ message: "Invalid student id" });
+        return;
+    }
+    const fileRepo = new FileRepo('students.json');
+    const student = await fileRepo.getStudentById(id);
+    if (student) {
+        res.status(404).json({ message: "Student not found" });
+        return;
+    }
+    res.status(200).json(student);
+});
+router.post('/api/students', async (req: Request, res:Response) => {
+    const { firstname, lastname } = req.body;
+    const newStudent: Student = {
+        id: Date.now(),
+        firstname,
+        lastname,
+        date: new Date()
+    };
+    const fileRepo = new FileRepo('students.json');
+    await fileRepo.addStudent(newStudent);
+    res.status(201).json(newStudent);
+});
+
+
 export default router;
