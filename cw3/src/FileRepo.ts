f50ddbbc81d2bd4c2c75b86cca241889be6c52bd
herbert3456 <hubert.bochenekty@interia.pl>
2026-03-28 08:42:19 +0100

    kk

--- cw3/src/FileRepo.ts
+++ cw3/src/FileRepo.ts
@@ -3,10 +3,14 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const pathToPublic = path.join(__dirname,'..', 'public');
+//klasa do obsługi plików JSON
 export class FileRepo {
     private pathToFile: string;
     constructor(fileName: string) {
         this.pathToFile = path.join(pathToPublic, fileName);
 }
+getfile(){
+    return this.pathToFile;
+} 
 
 }
--- cw3/src/router.ts
+++ cw3/src/router.ts
@@ -2,6 +2,7 @@ import { Router,type Request,type Response } from "express";
 import fs from 'fs/promises';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import { FileRepo } from "./FileRepo.js";
 const router = Router();
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const pathToPublic = path.join(__dirname,'..', 'public');
@@ -18,6 +19,8 @@ router.get('/index', async (req: Request, res:Response) => {
     res.status(200).send(context);
 });
 router.get('/api/students', async (req: Request, res:Response) => {
+    const fileRepo = new FileRepo('students.json');
+    console.log(fileRepo.getfile());
     const students = await fs.readFile(path.join(pathToPublic, 'students.json'), "utf-8");
     res.status(200).json(JSON.parse(students));
 });
